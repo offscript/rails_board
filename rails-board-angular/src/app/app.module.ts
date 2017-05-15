1ae0fd25a917183fa8e423f09898bbdd75f00dc2
Thomas Keating <twkeating1@gmail.com>
2017-05-15 13:53:31 -0400

    Authorization

---
+++ rails-board-angular/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'angular2-materialize';
+import { Angular2TokenService } from 'angular2-token';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { AuthService } from "./services/auth.service";
+import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from "./guards/auth.guard";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    AuthDialogComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    MaterializeModule
+  ],
+  providers: [ Angular2TokenService, AuthService, AuthGuard ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
