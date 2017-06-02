3820a2cb3ff2f8b2d75ba5481a482b97ec865494
Thomas Keating <twkeating1@gmail.com>
2017-06-02 14:08:22 -0400

    update db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,8 @@
 
 
 User.create(email: 'user@example.com', nickname: 'UOne', name: 'User One', password: "monkey67")
-Post.create(title: 'Software Developer', location: 'New York City, New York', description: 'Looking for a Software developer...')
+Post.create(title: 'Software Developer', location: 'New York City, New York', description: 'Looking for a Software Developer to oversee database')
+Post.create(title: 'Front-End Developer', location: 'Washington, D.C.', description: 'Looking for a Front-End Software Developer to manage web app. ')
+Post.create(title: 'Software Developer II', location: 'Seattle, Washington', description: 'NEED HELP NOW')
+Post.create(title: 'Senior Software Engineer', location: 'Austin, TX', description: 'Looking for a Senior Engineer to oversee product development')
+Post.create(title: 'Lead Web Developer', location: 'Atlanta, GA', description: 'Looking for experienced web developer to lead team')
